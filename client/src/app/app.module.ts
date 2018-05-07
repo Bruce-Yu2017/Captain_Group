ae2c4c8094d73464551b075ba5f66751b033cf9e
Bruce-Yu2017 <ilywanzi@gmail.com>
2018-05-07 14:25:52 -0700

    new

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { DisplayComponent } from './display/display.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { EventFormComponent } from './event-form/event-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeaderComponent,
+    DisplayComponent,
+    CalendarComponent,
+    EventFormComponent
   ],
   imports: [
     BrowserModule,
