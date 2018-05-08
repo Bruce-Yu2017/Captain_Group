1211a50c86a92dc0a0fc2b3aa027e0575f0e6793
emily <yuuamelie@gmail.com>
2018-05-07 17:58:20 -0700

    em

--- client/src/app/calendar/calendar.component.ts
+++ client/src/app/calendar/calendar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var jquery:any;
+declare var $ :any;
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
@@ -10,6 +11,28 @@ export class CalendarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
+      var date = new Date();
+  
+      $('#calendar').fullCalendar({
+        editable: true,
+        weekMode: 'liquid',
+        url: '#',
+        eventClick: function(e) {
+          alert(e.title);
+        },
+        events: [{
+          title: 'Project start',
+          start: new Date(2018, 4, 7, 9, 0o0),
+          end: new Date(2018, 4, 7, 10, 0o0),
+          allDay: false
+        }],
+  
+        dayClick: function() {
+          alert('a day has been clicked!');
+        }
+      });
+    
   }
 
 }
