8f038288e93b0d44b26d32bc84f95091910eed3d
zhen qiu <youthrone@hotmail.com>
2018-05-07 20:06:29 -0700

    events function

--- client/src/app/main.service.ts
+++ client/src/app/main.service.ts
@@ -1,8 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import io from "socket.io-client";
+import {BehaviorSubject} from 'Rxjs';
 
 @Injectable()
 export class MainService {
+  currentUser = null;
+  all_events: BehaviorSubject<any []> = new  BehaviorSubject([]);
 
-  constructor() { }
+  constructor(private _http:Http) { }
+
+  createStudentEvent(id, event, callback){
+    this._http.post('/studentevents', {id:id, event: event}).subscribe(
+      (res)=>{
+        callback(res.json());
+        let allEvents = res.json();
+        this.updateAllEvents(allEvents);
+      },(err)=>{
+        console.log("error 1 ");
+      }
+    )
+  };
+
+  createCaptainEvent(id, event, callback){
+    this._http.post('/captainevents', {id:id, event: event}).subscribe(
+      (res)=>{
+        callback(res.json());
+        let allEvents = res.json();
+        this.updateAllEvents(allEvents);
+      },(err)=>{
+        console.log("error 2 ");
+      }
+    )
+  };
+
+  updateAllEvents(data){
+    this.all_events.next(data);
+  }
+
 
 }
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,4 +1,7 @@
 var mainroutes = require('../controllers/mainControl.js');
+var studentEvent = require('../controllers/studentController');
+var captainEvent = require('../controllers/captainController');
+var events = require('../controllers/eventController');
 var path = require('path');
 
 
@@ -6,10 +9,25 @@ module.exports = function(app){
   //register
   app.post("/register", function (req, res) {
     mainroutes.register(req, res);
-  })
+  });
   //login
   app.post("/login", function (req, res) {
     mainroutes.login(req, res);
-  })
+  });
 
-}
+  // create student event
+  app.post('/studentevents', function(req, res){
+    studentEvent.create(req, res);
+  });
+
+  // create captain event
+  app.post('/captainevents', function(req, res){
+    captainEvent.create(req, res);
+  });
+
+  // get all events
+  app.get('/allevents', function(req, res){
+    events.getAllEvents(req, res);
+  });
+
+};
