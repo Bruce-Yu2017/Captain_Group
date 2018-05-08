7858e1149ded01546750c0e0e7db99eb0ecbf68b
Kevin Ke <kevinke@MacBook-Pro-1023.local>
2018-05-07 17:50:46 -0700

    new

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: NavbarComponent },
+  { path:'home', component: HeaderComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
