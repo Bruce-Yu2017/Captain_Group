9f6f9e7b66b2aa3a54f09c82928af44f2aa3c179
John Bradley <johnhavenbradley@sappercoding.com>
2018-05-13 19:23:25 -0700

    update

--- client/src/app/main.service.ts
+++ client/src/app/main.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import io from "socket.io-client";
-import { BehaviorSubject } from 'Rxjs';
+import { BehaviorSubject } from 'Rxjs/BehaviorSubject';
 
 @Injectable()
 export class MainService {
